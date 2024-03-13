54ea296b85df1fbaa6eb9afe51c88385a0767f67
Kenneth C <kennethalonsoc@gmail.com>
2024-03-13 12:37:19 -0600

    Revert "Add new Console.Log"
    This reverts commit 0b013c48194c1ddbe87b648cce62534e2f2ff2c7.

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 console.log("Hello, world!");
 console.log("Hello, world!!!!!!!!");
 console.log("Hello, world!!!!!!!!!!!!!!!!!!!!!!!!!!");
+=======
+console.log("Hello, world!");
+>>>>>>> parent of 0b013c4 (Add new Console.Log)
